9df4d3627c13b4e4e1b19d9cf4293e92a0d53730
Rodrigo Peleias <rodrigo.peleias@picpay.com>
2021-02-01 09:52:20 -0300

    ensure EmailValdiator calls validator with correct email

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -21,4 +21,10 @@ describe('Email Validator', () => {
     const isEmailValid = sut.isValid('invalid_email@mail.com')
     expect(isEmailValid).toBe(false)
   })
+
+  test('Should callvalidator with correct email', () => {
+    const sut = makeSut()
+    sut.isValid('any_email@mail.com')
+    expect(validator.email).toBe('any_email@mail.com')
+  })
 })
