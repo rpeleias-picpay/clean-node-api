cff4ac0f98327a09e327ccebb433b57dbe7e1768
Rodrigo Peleias <rodrigo.peleias@picpay.com>
2021-02-02 09:21:57 -0300

    ensure AuthUseCase return null if an invalid password is provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -5,9 +5,11 @@ const makeSut = () => {
   class LoadUserByEmailRepositorySpy {
     async load (email) {
       this.email = email
+      return this.user
     }
   }
   const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
+  loadUserByEmailRepositorySpy.user = {}
   const sut = new AuthUseCase(loadUserByEmailRepositorySpy)
   return {
     sut,
@@ -46,9 +48,16 @@ describe('Auth UseCase', () => {
     expect(promise).rejects.toThrow()
   })
 
-  test('Should return null if LoadUserByEmailRepository return null', async () => {
-    const { sut } = makeSut()
+  test('Should return null an invalid email is provided', async () => {
+    const { sut, loadUserByEmailRepositorySpy } = makeSut()
+    loadUserByEmailRepositorySpy.user = null
     const accessToken = await sut.auth('invalid_email@email.com', 'any_password')
-    expect(accessToken).toBe(null)
+    expect(accessToken).toBeNull()
+  })
+
+  test('Should return null an invalid password is provided', async () => {
+    const { sut } = makeSut()
+    const accessToken = await sut.auth('valid_email@email.com', 'invalid_password')
+    expect(accessToken).toBeNull()
   })
 })
