c8ced8f4411bbbd3c225c94916ca07f52a6d3a6b
Rodrigo Peleias <rodrigo.peleias@picpay.com>
2021-02-06 18:19:13 -0300

    move mongodb setup to a separate file

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -1,8 +1,6 @@
-const { MongoClient } = require('mongodb')
-
-let client, db
-
+const MongoHelper = require('../helpers/mongo-helper')
 const LoadUserByEmailRepository = require('./load-user-by-email-repository')
+let db
 
 const makeSut = () => {
   const userModel = db.collection('users')
@@ -15,11 +13,8 @@ const makeSut = () => {
 
 describe('LoadUserByEmail Repository', () => {
   beforeAll(async () => {
-    client = await MongoClient.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
-    db = client.db()
+    await MongoHelper.connect(process.env.MONGO_URL)
+    db = await MongoHelper.getDb()
   })
 
   beforeEach(async () => {
@@ -27,7 +22,7 @@ describe('LoadUserByEmail Repository', () => {
   })
 
   afterAll(async () => {
-    await client.close()
+    await MongoHelper.disconnect()
   })
 
   test('Should return null if no user is found', async () => {
