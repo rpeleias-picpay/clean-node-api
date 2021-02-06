4f47de732e6eafd0a80963ce82eb5a693f9d7eb9
Rodrigo Peleias <rodrigo.peleias@picpay.com>
2021-02-06 17:51:42 -0300

    ensure LoadUserByEmailRepository returns an user if user is found

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
-  collectCoverageFrom: ['**/**/*.js']
+  collectCoverageFrom: ['**/**/*.js'],
+  preset: '@shelf/jest-mongodb'
+
 }
