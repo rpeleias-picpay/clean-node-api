20e7045c6e6b1b21129e34f27f089176c0da8a17
Rodrigo Peleias <rodrigo.peleias@picpay.com>
2021-02-07 14:57:26 -0300

    ensure requests return given content-type when foorced

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -4,10 +4,20 @@ const app = require('../config/app')
 describe('Content-Type Middleware', () => {
   test('Should return json content type as default', async () => {
     app.get('/test_content_type', (req, res) => {
-      res.send({})
+      res.send('')
     })
     await request(app)
       .get('/test_content_type')
       .expect('content-type', /json/)
+  })
+
+  test('Should return xml content type if forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
   })
 })
