9146dd9554d15f11b2a31ef2e3a8b3e37ae425d2
Rodrigo Peleias <rodrigo.peleias@picpay.com>
2021-02-08 09:14:56 -0300

    add routes file

--- src/main/config/app.js
+++ src/main/config/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const setupApp = require('./setup')
+const setupRoute = require('./routes')
 
 setupApp(app)
+setupRoute(app)
 
 module.exports = app
